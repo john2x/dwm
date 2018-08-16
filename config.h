adf0f2f7f50d37e5ba3ffdd42a028462a7eecc0b
John Louis Del Rosario <john2x@gmail.com>
2018-08-16 15:01:39 +0800

    Revert to PragmataPro

--- config.h
+++ config.h
@@ -4,9 +4,9 @@
 /* appearance */
 
 static const char *fonts[] = {
-	"Siji:style=Regular:size=10"
+	"PragmataPro:style=Regular:size=9"
 };
-static const char dmenufont[]       = "Siji:style=Regular:size=10";
+static const char dmenufont[]       = "PragmataPro:style=Regular:size=9";
 static const char normbordercolor[] = "#988D6D";
 static const char normbgcolor[]     = "#FFFFE8";
 static const char normfgcolor[]     = "#988D6D";
@@ -19,7 +19,7 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 
 /* systray */
-static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
+static const unsigned int systraypinning = 1;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
 static const unsigned int systrayspacing = 2;   /* systray spacing */
 static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
 static const int showsystray        = 1;     /* 0 means no systray */
@@ -53,9 +53,9 @@ static const int resizehints = 1;    /* 1 means respect size hints in tiled resi
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
-	{ "",      tile },    /* first entry is default */
-	{ "",      NULL },    /* no layout function means floating behavior */
-	{ "",      monocle },
+	{ "",      tile },    /* first entry is default */
+	{ "",      NULL },    /* no layout function means floating behavior */
+	{ "",      monocle },
 };
 
 /* key definitions */
